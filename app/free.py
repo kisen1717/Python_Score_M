847acf1d6314b6e0914ac784864f3e537e58b719
KISENTAKUMU <hataosyurreck@gmail.com>
2025-04-16 15:35:46 +0900

    cda

--- app/free.py
+++ app/free.py
@@ -2,17 +2,27 @@
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 
-#一局ごと登録処理
+# 一局ごと登録処理
 def handle_free(mysql):
+    
+    cursor = mysql.connection.cursor()
+
+    # ユーザーの雀荘リスト取得
+    user = current_user.username
+    cursor.execute("SELECT id, name FROM free_jannsou WHERE username = %s", (user,))
+    jannsou_list = cursor.fetchall()  # → [(1, '○○荘'), (2, '△△荘')]
+
     if request.method == "POST":
         rank = request.form["rank"]
+        jannsou_id = request.form["jannsou"]  # 選択された雀荘のIDを取得
         user = current_user.username
 
-        cursor = mysql.connection.cursor()
-        query = "INSERT INTO free_results_all (username, juni) VALUES (%s, %s)"
-        cursor.execute(query, (user, rank))
+        # free_results_all にデータを挿入
+        query = "INSERT INTO free_results_all (username, juni, jansou) VALUES (%s, %s, %s)"
+        cursor.execute(query, (user, rank, jannsou_id))
         mysql.connection.commit()
-        cursor.close()
         flash("登録完了", "success")
+        return redirect(url_for("free"))
 
-    return render_template("free.html", username=current_user.username)
+    cursor.close()
+    return render_template("free.html", username=current_user.username, jannsou_list=jannsou_list)
